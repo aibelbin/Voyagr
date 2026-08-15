7433b237297482a42182fcb1e2e403dcecfd6923
Aibel Bin Zacariah <aibelbinzacariah@gmail.com>
2026-08-15 18:55:48 +0530

    feat(voyagr): add origin, dates and budget to Start Trip

--- packages/nodes-base/nodes/Voyagr/TripStart/TripStart.node.ts
+++ packages/nodes-base/nodes/Voyagr/TripStart/TripStart.node.ts
@@ -30,12 +30,67 @@ export class TripStart implements INodeType {
 				type: 'notice',
 				default: '',
 			},
+			{
+				displayName: 'Starting From',
+				name: 'startLocation',
+				type: 'string',
+				default: 'Home',
+				placeholder: 'Home',
+				description: 'Where the trip begins',
+			},
+			{
+				displayName: 'Trip Starts',
+				name: 'startDate',
+				type: 'dateTime',
+				default: '',
+				description: 'The day you leave',
+			},
+			{
+				displayName: 'Trip Ends',
+				name: 'endDate',
+				type: 'dateTime',
+				default: '',
+				description: 'The day you get back',
+			},
+			{
+				displayName: 'Budget',
+				name: 'budget',
+				type: 'number',
+				default: 0,
+				description: 'Total budget for the trip',
+			},
+			{
+				displayName: 'Currency',
+				name: 'currency',
+				type: 'options',
+				default: 'USD',
+				options: [
+					{ name: 'Australian Dollar (AUD)', value: 'AUD' },
+					{ name: 'British Pound (GBP)', value: 'GBP' },
+					{ name: 'Canadian Dollar (CAD)', value: 'CAD' },
+					{ name: 'Euro (EUR)', value: 'EUR' },
+					{ name: 'Indian Rupee (INR)', value: 'INR' },
+					{ name: 'Japanese Yen (JPY)', value: 'JPY' },
+					{ name: 'UAE Dirham (AED)', value: 'AED' },
+					{ name: 'US Dollar (USD)', value: 'USD' },
+				],
+			},
 		],
 	};
 
 	async trigger(this: ITriggerFunctions): Promise<ITriggerResponse> {
 		const manualTriggerFunction = async () => {
-			this.emit([this.helpers.returnJsonArray([{}])]);
+			this.emit([
+				this.helpers.returnJsonArray([
+					{
+						startLocation: this.getNodeParameter('startLocation', '') as string,
+						startDate: this.getNodeParameter('startDate', '') as string,
+						endDate: this.getNodeParameter('endDate', '') as string,
+						budget: this.getNodeParameter('budget', 0) as number,
+						currency: this.getNodeParameter('currency', 'USD') as string,
+					},
+				]),
+			]);
 		};
 		return { manualTriggerFunction };
 	}
